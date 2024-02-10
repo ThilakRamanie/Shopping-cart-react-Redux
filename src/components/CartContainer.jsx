11eee009228910b80e5e81fa1f70ca2437dfacce
ThilakRamanie <thilakramanie007@gmail.com>
2024-02-10 22:15:54 +0000

    Add to cart --redux-code

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,47 @@
+import cartItems from "../cartItems";
+import { useSelector } from "react-redux";
+import CartItem from "./CartItem.jsx";
+import { clearCart } from "../features/cart/cartSlice.js";
+import { useDispatch } from "react-redux";
+
+const CartContainer = () => {
+  const dispatch = useDispatch();
+  const { amount, cartItems, total } = useSelector((store) => store.cart);
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your Bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your Bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            Total <span>${total.toFixed(2)}</span>
+          </h4>
+          <button
+            className="btn clear-btn"
+            onClick={() => dispatch(clearCart())}
+          >
+            Clear Cart
+          </button>
+        </div>
+      </footer>
+    </section>
+  );
+};
+export default CartContainer;
